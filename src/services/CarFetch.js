eda3b83c344c3197904977e5038cab224f043baf
eder <edra1791@gmail.com>
2021-11-23 14:04:00 -0500

    services fetch to api

--- src/services/CarFetch.js
+++ src/services/CarFetch.js
@@ -1,4 +1,4 @@
-export const CreateCarFetch = (data) => {
+export const CreateCar = (data) => {
   return fetch("http://localhost:3001/cars", {
     method: "POST",
     headers: {
@@ -14,9 +14,50 @@ export const CreateCarFetch = (data) => {
     });
 }
 
-export const ShowAllCarsFetch = () => {
-  fetch("http://localhost:3001/cars", {
+export const ShowAllCars = () => {
+  return fetch("http://localhost:3001/cars", {
     method: "GET",
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+export const ShowCarById = (id) => {
+  return fetch(`http://localhost:3001/cars/${id}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+
+export const DeleteCar = (id) => {
+  return fetch(`http://localhost:3001/cars/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+export const UpdateCar = (id, data) => {
+  return fetch(`http://localhost:3001/cars/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
   })
     .then((response) => {
       return response.json()
